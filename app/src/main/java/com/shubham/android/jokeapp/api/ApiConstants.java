e9d05d4c8193cc6a615ac8cf176642092ec08fde
Shubham Mishra <shubham@bajaar.com>
2018-04-12 15:33:46 +0530

    testing what will happen ,and added api constants file

---
+++ app/src/main/java/com/shubham/android/jokeapp/api/ApiConstants.java
@@ -0,0 +1,10 @@
+package com.shubham.android.jokeapp.api;
+
+/**
+ * API constants for containing the end points of the Chris Norris APIx
+ *
+ * Created by shubham on 12/04/18.
+ */
+
+public class ApiConstants {
+}
